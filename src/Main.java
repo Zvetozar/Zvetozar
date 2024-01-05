be3d161258d7f2795d8300f433c1fb3e04f89435
Ernazar <ernazar1609@gmail.com>
2024-01-05 22:14:58 +0600

    MyFirstCommit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        double height = 1.78;
+        int weight = 82;
+        double index = weight / (height * height);
+        System.out.println(index);
+
+
+        
+
+
+    }
+}
+
+
+
+
